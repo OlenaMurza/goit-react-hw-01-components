a1c7a6791cf85d40031bb90bbe4d46b94ea391ba
OlenaMurza <murzaelena@gmail.com>
2023-02-16 20:54:39 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,16 @@
-import user from 'components/user.json';
-// import data from 'data/data.json';
-// import friends from 'data/friends.json';
-// import transactions from 'data/transactions.json';
+import user from 'data/user.json';
+
 
 import { Profile } from './Profile/Profile';
-// import { FriendList } from './FriendList/FriendList';
-// import { Statistics } from './Statistics/Statistics';
-// import { TransactionsHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
-  return (
-    <>
-      <Profile
+  return (<Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
-     
-    </>
-  );
+      />);
 };
+
+
